e185d221aa213ccf424f17d39369d7406bb2985e
Sean Farrow <sean.farrow@rocksolidknowledge.com>
2023-06-28 09:40:02 +0100

    Add a set of page models for the IdentityServer login page.

---
+++ test/RSKDockerDemo.IntegrationTests/PageModels/LoginPage.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Playwright;
+
+namespace RskDemo.Ids.Tests.Integration.PageModels;
+
+public class LoginPage : PageBase
+{
+    private ILocator usernameField;
+    private ILocator passwordField;
+    private ILocator loginButton;
+
+    public LoginPage(IPage page, string address) : base(page, address)
+    {
+    }
+
+    public async Task EnterUsername(string username) => await usernameField.FillAsync(username);
+
+    public async Task EnterPassword(string password) => await passwordField.FillAsync(password);
+
+    public async Task ClickLogin() => await loginButton.ClickAsync();
+    
+    protected override void SetupLocators(IPage page)
+    {
+        usernameField = Page.GetByPlaceholder("Username");
+        passwordField = Page.GetByPlaceholder("Password");
+        loginButton = Page.Locator(@"button[value=""login""]");
+    }
+}
---
+++ test/RSKDockerDemo.IntegrationTests/PageModels/PageBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+
+namespace RskDemo.Ids.Tests.Integration.PageModels;
+
+public abstract class PageBase
+{
+    private readonly string address;
+    
+    protected IPage Page { get; }
+
+    protected PageBase(IPage page, string address)
+    {
+        Page = page;
+        this.address = address;
+    }
+
+    public async Task GotoAsync()
+    {
+        await Page.GotoAsync(this.address);
+        SetupLocators(Page);
+    }
+
+    protected abstract void SetupLocators(IPage page);
+    }
