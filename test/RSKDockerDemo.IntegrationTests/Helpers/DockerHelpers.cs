a85b639fe8d6972b227ff0058a5b580c57f06267
Sean Farrow <sean.farrow@rocksolidknowledge.com>
2023-06-28 12:48:06 +0100

    Add a helper method to detect whether code is running in a docker container.

---
+++ test/RSKDockerDemo.IntegrationTests/Helpers/DockerHelpers.cs
@@ -0,0 +1,12 @@
+﻿namespace RSKDockerDemo.IntegrationTests.Helpers;
+/// <summary>
+/// A set of helpers allowing us to detect information about Docker.
+/// </summary>
+public static class DockerHelpers
+{
+    /// <summary>
+    /// ARe we currently running in Docker.
+    /// </summary>
+    /// <returns><c>True</c> if the code is currently running in a Docker container, <c>False</c> otherwise.</returns>
+    public static bool InDocker() => Convert.ToBoolean(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"));
+}
