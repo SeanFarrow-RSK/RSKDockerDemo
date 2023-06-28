b465a17edc145ed31503f2a083abe54bf7cdb941
Sean Farrow <sean.farrow@rocksolidknowledge.com>
2023-06-28 09:32:49 +0100

    Add a SpecFlow feature to test that logging in to IdentityServer works as expected. also, ensure we ignore SpecFlow generated files.

---
+++ test/RSKDockerDemo.IntegrationTests/Features/IdentityServerLogIn.feature
@@ -0,0 +1,11 @@
+﻿Feature: IdentityServerLogIn
+
+ensure login works as expected.
+
+@RequiresChrome	
+Scenario: Login succeeds
+	Given I am on the login page
+	When I enter a username of alice
+	And I enter a password of alice
+	And I click the login button
+	Then I should be loged in
---
+++ test/RSKDockerDemo.IntegrationTests/StepDefinitions/IdentityServerLogInStepDefinitions.cs
@@ -0,0 +1,39 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace RSKDockerDemo.IntegrationTests.StepDefinitions
+{
+    [Binding]
+    public class IdentityServerLogInStepDefinitions
+    {
+        [Given(@"I am on the login page")]
+        public void GivenIAmOnTheLoginPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I enter a username of alice")]
+        public void WhenIEnterAUsernameOfAlice()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I enter a password of alice")]
+        public void WhenIEnterAPasswordOfAlice()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I click the login button")]
+        public void WhenIClickTheLoginButton()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should be loged in")]
+        public void ThenIShouldBeLogedIn()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
